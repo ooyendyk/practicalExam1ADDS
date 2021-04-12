0d7be63d18a77c1c7e0419152906f317f6851915
Nerd-1 <ooyendyk@tuta.io>
2021-04-12 13:57:02 +0930

    Add Transport.cpp, Transport.h

---
+++ Transport.cpp
@@ -0,0 +1,17 @@
+//
+// Created by user on 12/04/2021.
+//
+
+#include "Transport.h"
+
+Transport::Transport() {
+    dist_travelled = 0;
+}
+
+int Transport::get_dist_travelled() {
+    return dist_travelled;
+}
+
+//void Transport::go() {
+
+//}
---
+++ Transport.h
@@ -0,0 +1,19 @@
+//
+// Created by user on 12/04/2021.
+//
+
+#ifndef UNTITLED1_TRANSPORT_H
+#define UNTITLED1_TRANSPORT_H
+
+
+class Transport {
+private:
+    int dist_travelled;
+public:
+    Transport();
+    int get_dist_travelled();
+    void go();
+};
+
+
+#endif //UNTITLED1_TRANSPORT_H
